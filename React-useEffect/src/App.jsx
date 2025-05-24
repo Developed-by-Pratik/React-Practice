f2306c754374e6284e066153baffdaf28576437b
Developed-by-Pratik <pratikkakade4618@gmail.com>
2025-05-24 14:47:02 +0530

    Added useEffect

---
+++ React-useEffect/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Loading from './Loading'
+
+function App() {
+  return (
+    <div>
+      <h1>React useEffect</h1>
+      <Loading />
+    </div>
+  )
+}
+
+export default App
